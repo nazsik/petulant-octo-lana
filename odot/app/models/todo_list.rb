be54baf5ab88e571ce09d40782a254875f7e1f88
Nazar <nazarsikander@yahoo.com>
2014-06-13 15:47:25 -0700

    added min char and presence for both title and descript

--- odot/app/models/todo_list.rb
+++ odot/app/models/todo_list.rb
@@ -1,4 +1,4 @@
 class TodoList < ActiveRecord::Base
-	validates :title, presence: true
-	validates :title, length: { minimum: 3 }
+	validates :title, :description, presence: true
+	validates :title, :description, length: { minimum: 3 }
 end
